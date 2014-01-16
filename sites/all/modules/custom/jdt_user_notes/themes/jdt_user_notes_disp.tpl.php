7b647e3ca5cb513ebb7b37a56d10cbe4fa50c935
Paolo Montano <Paolo@Appnovation.com>
2014-01-16 13:46:46 -0800

    Changed PHP structure blocks.

--- sites/all/modules/custom/jdt_user_notes/themes/jdt_user_notes_disp.tpl.php
+++ sites/all/modules/custom/jdt_user_notes/themes/jdt_user_notes_disp.tpl.php
@@ -1,15 +1,16 @@
 <?php
 /**
  * @file
- * Template for displaying user notes.
+ * Template for JDT User Notes
  */
-  if (arg(0) == 'node' && is_numeric($jdt_req = arg(1))) {
-    print '<br />';
-    $jdt_notes_array = jdt_user_notes_get_notes($jdt_req);
-    foreach ($jdt_notes_array as $jdt_note) {
-      print '<p>' . $jdt_note[1] . '</p>';
-      print '<p><a href="./jdt_user_note_edit/' . $jdt_note[0] . '">edit this note</a></p>';
-      print '<br />';
-    }
-  }
 ?>
+
+<?php if (arg(0) == 'node' && is_numeric($jdt_req = arg(1))): ?>
+  <br />
+  <?php $jdt_notes_array = jdt_user_notes_get_notes($jdt_req); ?>
+  <?php foreach ($jdt_notes_array as $jdt_note): ?>
+    <p><?php print $jdt_note[1]; ?></p>
+    <p><a href="./jdt_user_note_edit/<?php print $jdt_note[0]; ?>">Edit this note</a></p>
+    <br />
+  <?php endforeach; ?>
+<?php endif; ?>
