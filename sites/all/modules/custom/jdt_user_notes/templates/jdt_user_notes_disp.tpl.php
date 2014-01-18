1419a2ecd432d9c7ca4ae3d8876fe9fb4edf61d0
Paolo Montano <Paolo@Appnovation.com>
2014-01-17 16:33:47 -0800

    Preparing for Ajax.

--- sites/all/modules/custom/jdt_user_notes/templates/jdt_user_notes_disp.tpl.php
+++ sites/all/modules/custom/jdt_user_notes/templates/jdt_user_notes_disp.tpl.php
@@ -9,4 +9,6 @@
   <br />
   <p><?php print $jdt_article['note_text']; ?></p>
   <p><?php print $jdt_article['note_link']; ?></p>
+  <div id="<?php print $jdt_article['note_div']; ?>"></div>
+
 <?php endforeach; ?>
