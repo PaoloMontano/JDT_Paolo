dae7419e637d2f29fdda2e713e7d8c09e328bdc0
Paolo Montano <Paolo@Appnovation.com>
2014-01-21 11:14:19 -0800

    Working Ajax form generation and submission.

--- sites/all/modules/custom/jdt_user_notes/templates/jdt_user_notes_disp.tpl.php
+++ sites/all/modules/custom/jdt_user_notes/templates/jdt_user_notes_disp.tpl.php
@@ -7,9 +7,9 @@
 
 <?php foreach ($jdt_articles as $jdt_article): ?>
   <br />
-  <p><?php print $jdt_article['note_text']; ?></p>
+
   <!-- <p><?php print $jdt_article['note_link']; ?></p> -->
-  <div id="<?php print $jdt_article['note_div']; ?>"></div>
+  <div id="<?php print $jdt_article['note_div']; ?>"><?php print $jdt_article['note_text']; ?></div>
   <div id="<?php print $jdt_article['note_ajax_div']; ?>">
     <?php print $jdt_article['note_ajax'] ?>
   </div>
