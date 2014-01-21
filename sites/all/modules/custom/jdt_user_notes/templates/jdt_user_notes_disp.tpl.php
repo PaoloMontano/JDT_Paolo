47eeaf53a80ce28b373b021f619e308ba0375588
Paolo Montano <Paolo@Appnovation.com>
2014-01-20 17:09:17 -0800

    Working AJAX. Need to do forms now.

--- sites/all/modules/custom/jdt_user_notes/templates/jdt_user_notes_disp.tpl.php
+++ sites/all/modules/custom/jdt_user_notes/templates/jdt_user_notes_disp.tpl.php
@@ -8,7 +8,11 @@
 <?php foreach ($jdt_articles as $jdt_article): ?>
   <br />
   <p><?php print $jdt_article['note_text']; ?></p>
-  <p><?php print $jdt_article['note_link']; ?></p>
+  <!-- <p><?php print $jdt_article['note_link']; ?></p> -->
   <div id="<?php print $jdt_article['note_div']; ?>"></div>
+  <div id="<?php print $jdt_article['note_ajax_div']; ?>">
+    <?php print $jdt_article['note_ajax'] ?>
+  </div>
+  <br />
 
 <?php endforeach; ?>
